d4577015d022a58ff2872fcc5344259a7d4173de
jinguangcai <jinguangcai@126.com>
2017-08-23 11:47:19 +0800

    ajax test

--- src/main/java/com/jiangsu/controller/CrawlerController.java
+++ src/main/java/com/jiangsu/controller/CrawlerController.java
@@ -2,20 +2,28 @@ package com.jiangsu.controller;
 
 
 import com.jiangsu.crawler.Crawler;
+import com.jiangsu.entity.Sale;
+import com.jiangsu.service.SaleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 /**
  * @author lepdou 2017-06-18
  */
 @RestController
+@CrossOrigin
 public class CrawlerController {
 
   @Autowired
   private Crawler crawler;
+  @Autowired
+  private SaleService saleService;
 
   @RequestMapping(value = "/crawler", method = RequestMethod.GET)
   public String crawler() {
@@ -28,7 +36,11 @@ public class CrawlerController {
   }
 
   @RequestMapping(value = "/hello", method = RequestMethod.GET)
-  public String test(){
-    return "hello world...";
+  public List<Sale> test(){
+    List<Sale> sales = saleService.find("主城区",1);
+    for(Sale sale:sales){
+      System.out.print(sale);
+    }
+    return sales;
   }
 }
